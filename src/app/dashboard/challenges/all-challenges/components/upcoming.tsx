9fefe28d56ffa34a4481d9cdb0984b1c5f020621
Vf-projects <omenihuakachukwuvictor@gmail.com>
2026-03-18 16:33:48 +0100

    worked on the feature

---
+++ src/app/dashboard/challenges/all-challenges/components/upcoming.tsx
@@ -0,0 +1,83 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { Box, Text, SimpleGrid, Image, Button } from '@chakra-ui/react';
+import Challenges from './challenges';
+
+interface ChallengeType {
+  challenge: string;
+  properties: {
+    type: string;
+    name: string;
+    participants: string;
+  }[];
+}
+
+export default function Upcoming() {
+  const [challenges, setChallenges] = useState<ChallengeType[]>([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setChallenges(Challenges);
+    }, 1000);
+  }, []);
+
+  // Filter challenges with challenge === 'Ongoing'
+  const filterOngoing = challenges.filter(c => c.challenge === 'Upcoming');
+
+  return (
+    <Box display="grid" color="#2E3338">
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={6}>
+        {filterOngoing.length === 0 ? (
+          <Text>No Challenge</Text>
+        ) : (
+          filterOngoing.flatMap(c =>
+            c.properties.map((item) => (
+              <Box key={item.name} border='1px solid #8F98A3' p='20px' borderRadius='20px'>
+
+                <Text fontSize='18px' fontWeight='bold'>
+                  {item.name}
+                </Text>
+
+                <Box display='flex' alignItems='center' mt='10px'>
+                  <Image src='/people.svg' alt='people icon' w='24px' h='19px' />
+                  <Text ml='5px'>
+                    {item.participants}
+                  </Text>
+                </Box>
+
+                <Box display='flex' alignItems='center' mt='20px'>
+
+                  <Button
+                    bg={c.challenge === 'Ongoing' ? 'blue.800' : '#ABB2BA'}
+                    color={c.challenge === 'Ongoing' ? 'white' : '#1D347C'}
+                    w='100px'
+                    h='26px'
+                    borderRadius='16px'
+                    _hover={{}}
+                  >
+                    {c.challenge}
+                  </Button>
+
+                  <Button
+                    ml='5px'
+                    bg='none'
+                    border='1px solid #8F98A3'
+                    color='#2E3338'
+                    w='100px'
+                    h='26px'
+                    borderRadius='16px'
+                    _hover={{}}
+                  >
+                    {item.type}
+                  </Button>
+
+                </Box>
+              </Box>
+            ))
+          )
+        )}
+      </SimpleGrid>
+    </Box>
+  );
+}
--- src/app/dashboard/challenges/setup-challenges/page.tsx
+++ src/app/dashboard/challenges/setup-challenges/page.tsx
@@ -1,9 +1,95 @@
-import { Box } from '@chakra-ui/react';
+'use client'
+
+import { Box, Text, Input, Textarea, Button } from '@chakra-ui/react';
+
+
 export default function SetupChallenges() {
+  
   return (
     <Box display={'flex'} flexDirection={'column'} gap={6}>
       <Box>
-        <h1>Setup Challenges</h1>
+        <Text fontSize='32px' fontWeight='bold'>
+          Set Up Challenges
+        </Text>
+        <Text color='#70777F' fontSize='16px'>
+          Create a new challenge for participants
+        </Text>
+
+        <Box border='1px solid #F7F6F6' w='100%' h='auto' p='32px' rounded='lg' mt='24px'>
+          <Text fontSize='24px' fontWeight='bold'>
+            Challenge Details
+          </Text>
+          
+          <Text color='#595959' mt='24px' mb='5px'>Challenge Title</Text>
+          <Input 
+            type='text'
+            placeholder='Enter Challenge Title'
+            w='100%'
+            border='1px solid #CBCBCB'
+            _focus={{
+              outline: 'none',
+              boxShadow: 'none',
+            }}
+          />
+          
+          <Text color='#595959' mt='24px' mb='5px'>Description</Text>
+          <Textarea 
+            placeholder='Describe the Challenge objectives and rules'
+            w='100%'
+            h='121px'
+            border='1px solid #CBCBCB'
+            _focus={{
+              outline: 'none',
+              boxShadow: 'none',
+            }}
+          />
+
+          <Box mt='24px' display='flex' justifyContent='space-between' alignItems='center' gap='50px'>
+            <Box w='50%'>
+              <Text color='#595959' mb='5px'>Start date</Text>
+              <Input
+                type='date'
+                pl='40px'
+                w='full'
+                border='1px solid #CBCBCB'
+                _focus={{
+                  outline: 'none',
+                  boxShadow: 'none',
+                }}
+              />
+            </Box>
+            
+            <Box w='50%'>
+              <Text color='#595959' mb='5px'>End date</Text>
+              <Input
+                type='date'
+                pl='40px'
+                w='full'
+                border='1px solid #CBCBCB'
+                _focus={{
+                  outline: 'none',
+                  boxShadow: 'none',
+                }}
+              />
+            </Box>
+          </Box>
+
+          <Text color='#595959' mt='24px' mb='5px'>Max Participants</Text>
+          <Input 
+            type='text'
+            placeholder='Enter Maximum number of participants'
+            w='100%'
+            border='1px solid #CBCBCB'
+            _focus={{
+              outline: 'none',
+              boxShadow: 'none',
+            }}
+          />
+
+          <Button w='100%' mt='24px' mb='5px'>
+            Create Challenge
+          </Button>
+        </Box>
       </Box>
     </Box>
   );
