9637ed1bab385265938f5d48277aa34e834f2948
Josseycodes <adewumijosephine1@gmail.com>
2026-04-20 10:52:48 +0100

    Fixed vercel deployment bug.

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { Metadata } from 'next';
 import SignUpClient from './components/SignUpClient';
 
@@ -7,5 +8,9 @@ export const metadata: Metadata = {
 };
 
 export default function SignUpPage() {
-  return <SignUpClient />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <SignUpClient />
+    </Suspense>
+  );
 }
